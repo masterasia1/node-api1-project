ab4cb3fb8e4bd14089e413cc9e229efca252187b
John Dee <johndeebdd@gmail.com>
2021-12-09 20:19:56 -0800

    complete

--- api/server.js
+++ api/server.js
@@ -1,3 +1,27 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
+const server = express()
+const User = require('./users/model')
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.get ('/api/users', (req, res)=>{
+   User.find()
+    .then(users => {
+        res.json(users)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'err',
+            err: err.message
+        })
+    })
+})
+
+
+
+
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: 'not found'
+    })
+})
+module.exports = server; // EXPORT YOUR SERVER instead of {}
