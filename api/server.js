5352c1576d79e38bd80bef74941d3f02457c24d4
masterasia1 <a.mayfield18@gmail.com>
2021-12-09 20:33:57 -0800

    testing

--- api/server.js
+++ api/server.js
@@ -3,6 +3,47 @@ const express = require('express')
 const server = express()
 const User = require('./users/model')
 
+server.use(express.json())
+
+
+
+
+
+server.delete('/api/users/:id', async (req, res) => {
+const possibleUser= await User.findById (req.params.id)
+if (!possibleUser){
+    res.status(404).json({
+        message: 'h'
+    })
+}else
+{
+    const deletedUser= await User.remove(possibleUser.id)
+    res.status(200).json(deletedUser)
+}
+})
+
+//test testing 
+
+
+server.post('/api/users', (req, res) => {
+const user= req.body;
+if (!user.name || !user.bio){
+    res.status(422).json({
+        message: 'h'
+    })
+} else {
+    User.insert(user)
+    .then(stuff => {
+
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'err',
+            err: err.message
+        })
+    })
+}
+})
 server.get ('/api/users', (req, res)=>{
    User.find()
     .then(users => {
@@ -17,7 +58,24 @@ server.get ('/api/users', (req, res)=>{
 })
 
 
-
+server.get ('/api/users/:id', (req, res)=>{
+    User.findById(req.params.id)
+     .then(user => {
+         if (!user) {
+             res.status(404).json({
+                 message: "c"
+             })
+         }
+        res.json(user)
+     })
+     .catch(err => {
+         res.status(500).json({
+             message: 'err',
+             err: err.message
+         })
+     })
+ })
+ 
 
 server.use('*', (req, res) => {
     res.status(404).json({
